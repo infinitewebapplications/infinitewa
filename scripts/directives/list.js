a650942694034b55c4a5ad55f5a01f3830638b17
Caleb Nance <calebnance@gmail.com>
2016-11-21 10:47:15 -0500

    stop double controller declaration..

--- scripts/directives/list.js
+++ scripts/directives/list.js
@@ -4,7 +4,7 @@ angular.module('iwa')
 .directive('list', function() {
 
   return {
-    controller : 'mainCtrl', // set controller specifically.. get rid of ng-controller
+    // controller : 'mainCtrl', // set controller specifically.. get rid of ng-controller
     replace : true, // inject the html, get rid of custom tags
     templateUrl : 'templates/list.html' // set template html
   };
